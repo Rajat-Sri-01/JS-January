5e562c345d71e780087a5aab6d317ee67b4c5483
JMD-04 <155756716+JMD-04@users.noreply.github.com>
2024-01-07 16:43:21 +0000

    Datatypes (Null, Undefined, Number...)

---
+++ 01_Basics/01_datatypes.js
@@ -0,0 +1,26 @@
+"use strict" 
+// (its used to use new engines fucntions of javascript)
+
+// alert( 3 +3 )
+// (alert is used in Node.js not in browser engines)
+
+let name = "Rajat"
+// String
+let age = 25
+// Int or Number
+let accountNumber = 12345678901234576
+// Big Int
+let creditcardDetails = false
+let isloggedIn = true
+// Boolean 
+let state = null;
+let country;
+
+console.log(typeof "Rajat");
+console.log(typeof age);
+console.log(typeof state);
+console.log(typeof country);
+
+
+console.log(typeof undefined); //Output is Undefined
+console.log(typeof null); //Output is Object

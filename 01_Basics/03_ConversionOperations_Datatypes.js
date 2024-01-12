ef674d29a6373ec57cb44f90152639d5903176aa
JMD-04 <155756716+JMD-04@users.noreply.github.com>
2024-01-12 14:16:27 +0000

    conversion operations datatypes

---
+++ 01_Basics/03_ConversionOperations_Datatypes.js
@@ -0,0 +1,30 @@
+let number = "28rajat"
+let valueisNumber = Number(number)
+console.log(typeof valueisNumber);
+console.log(valueisNumber); 
+// Nan = Not a Number.
+
+let score = null
+console.log(typeof score);
+console.log(typeof (score));
+// Output is Object
+
+let somestring = "rajat"
+let valueisString = Boolean(somestring)
+// console.log(typeof valueisString);
+// console.log(valueisString);
+
+let someboolean = 1
+let valueisBoolean = String(someboolean)
+// console.log(typeof valueisBoolean);
+// console.log(valueisBoolean);
+
+let someboolean2 = undefined
+let valueisBoolean2 = Number(someboolean2)
+// console.log(typeof valueisBoolean2);
+// console.log(valueisBoolean2);
+
+let someboolean3 = null
+let valueisBoolean3 = String(someboolean3)
+console.log(typeof valueisBoolean3);
+console.log(valueisBoolean3);

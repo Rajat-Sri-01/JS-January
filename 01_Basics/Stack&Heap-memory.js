87605c2672c69dc5482ff21874726cab937a4b3b
JMD-04 <155756716+JMD-04@users.noreply.github.com>
2024-04-12 10:23:48 +0000

    stack heap memory

---
+++ 01_Basics/Stack&Heap-memory.js
@@ -0,0 +1,24 @@
+// STACK ( PRIMITIVE )
+// HEAP (NON-PRIMITIVE)
+
+let city = "Kanpur"
+let newcity = city
+
+console.log(city);
+console.log(newcity);
+
+
+
+
+
+
+let state = {
+    onecity: "jaipur"
+}
+
+let secondstate=state
+
+state.onecity = "noida"
+
+console.log(state.onecity);
+console.log(secondstate.onecity);
